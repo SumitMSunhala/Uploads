29e33d2c8660caac11cc9c713a1357863ac5c6bf
Ankur <ansupatel86@gmail.com>
2018-03-30 22:49:11 +0530

    make new lookups

--- GlobalHRMSApi/GlobalHRMSApi/Controllers/LookupController.cs
+++ GlobalHRMSApi/GlobalHRMSApi/Controllers/LookupController.cs
@@ -50,5 +50,76 @@ namespace GlobalHRMSApi.Controllers
         {
             return lookupLogic.GetReligions(id).ToList();
         }
+
+//        CONTRACTOR
+//COMPANYAPPOINTMENT TYPE
+//APPOINTMENT TYPE
+//DESGINATION
+//CATEGORY
+//GRADE
+//DEPARTMENT
+//UNIT
+
+//MARITAL STATUS
+
+//RELIGION
+//BLOOD GROUP
+//SEX
+//EDUCATION
+
+//MODE OF PAYMENT
+//BANK
+//BRANCH OF THE BANK
+
+//RELATION
+
+        [Route("appointmentTypes/{id?}")]
+        public List<AppointmentType> GetAppointmentTypes(int? id = null)
+        {
+            return lookupLogic.GetAppointmentTypes(id).ToList();
+        }
+
+        [Route("banks/{id?}")]
+        public List<Bank> GetBanks(int? id = null)
+        {
+            return lookupLogic.GetBanks(id).ToList();
+        }
+
+        [Route("categories/{id?}")]
+        public List<Category> GetCategories(int? id = null)
+        {
+            return lookupLogic.GetCategories(id).ToList();
+        }
+
+        [Route("designations/{id?}")]
+        public List<Designation> GetDesignations(int? id = null)
+        {
+            return lookupLogic.GetDesignations(id).ToList();
+        }
+
+        [Route("educations/{id?}")]
+        public List<Education> GetEducations(int? id = null)
+        {
+            return lookupLogic.GetEducations(id).ToList();
+        }
+
+        [Route("grades/{id?}")]
+        public List<Grade> GetGrades(int? id = null)
+        {
+            return lookupLogic.GetGrades(id).ToList();
+        }
+
+        [Route("relations/{id?}")]
+        public List<Relation> GetRelations(int? id = null)
+        {
+            return lookupLogic.GetRelations(id).ToList();
+        }
+
+        [Route("units/{id?}")]
+        public List<Unit> GetUnits(int? id = null)
+        {
+            return lookupLogic.GetUnits(id).ToList();
+        }
+
     }
 }
